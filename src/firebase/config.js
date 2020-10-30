de1f80497a407bfa50d5e481edcb6280b9deea42
EvanKelly831 <49172597+EvanKelly831@users.noreply.github.com>
2020-10-30 15:41:20 -0400

    finished

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -6,13 +6,13 @@ import 'firebase/firestore';
 // Your web app's Firebase configuration
   // Your web app's Firebase configuration - Use the config settings from your firebase project
   var firebaseConfig = {
-    apiKey: "********************",
-    authDomain: "*******************",
-    databaseURL: "*******************",
-    projectId: "********************",
-    storageBucket: "******************",
-    messagingSenderId: "*****************",
-    appId: "*************************"
+    apiKey: "AIzaSyDLmnoBvnjOQ-AaJgDw1DnVBiTETbcp8LI",
+    authDomain: "the-net-ninja-sandbox.firebaseapp.com",
+    databaseURL: "https://the-net-ninja-sandbox.firebaseio.com",
+    projectId: "the-net-ninja-sandbox",
+    storageBucket: "the-net-ninja-sandbox.appspot.com",
+    messagingSenderId: "485942827092",
+    appId: "1:485942827092:web:1811d9d8f1f5fabcd5b5c1"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
